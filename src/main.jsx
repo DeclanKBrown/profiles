e38aee65c46926bd46f6e2ecbde2663c6f9c7743
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-09-18 10:12:16 +1000

    Added loader route

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import Root from './routes/Root'
 import ErrorPage from './Error-page'
+import Contact, { loader as contactLoader } from "./routes/Contact";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/Root"
+
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    errorElement: <ErrorPage />
-  }
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ]
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
